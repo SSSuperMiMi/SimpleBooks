3210977c0e3abb3648ccacf01967a618e93f3cd1
ahkfjhsa <bingxv@yeah.net>
2015-04-28 10:31:53 +0800

    Initial Commit

---
+++ Simple Books/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Simple Books
+//
+//  Created by Allen Zhao on 4/28/15.
+//  Copyright (c) 2015 Apple Orchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Simple Books/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Simple Books
+//
+//  Created by Allen Zhao on 4/28/15.
+//  Copyright (c) 2015 Apple Orchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
